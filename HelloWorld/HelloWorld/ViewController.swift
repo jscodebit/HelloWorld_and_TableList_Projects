7d728857da1232c2a9762de1eb3e9a432c7cb407
jscodebit <soujanya.janapatla@gmail.com>
2020-09-24 18:02:20 -0500

    Commiting the initial template of Hello World Application in Xcode

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by student on 9/24/20.
+//  Copyright © 2020 NorthWest. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+
+
+}
+
