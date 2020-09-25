1bf9466f9c18492e4d0bb7025de3639848ebb96b
jscodebit <soujanya.janapatla@gmail.com>
2020-09-24 21:11:54 -0500

    Initial commit to Exercise 02

---
+++ HelloWorldCustomMessage/HelloWorldCustomMessage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorldCustomMessage
+//
+//  Created by student on 9/24/20.
+//  Copyright © 2020 NorthWest. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func showMessage(sender: UIButton) {
+        let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello World", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+
+}
+
