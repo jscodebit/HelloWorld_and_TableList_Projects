a7ebf33c73d060ec97853d0b2509b0d679fc953f
jscodebit <soujanya.janapatla@gmail.com>
2020-09-24 21:37:41 -0500

    Made some indentation to the code

--- HelloWorldCustomMessage/HelloWorldCustomMessage/ViewController.swift
+++ HelloWorldCustomMessage/HelloWorldCustomMessage/ViewController.swift
@@ -16,9 +16,29 @@ class ViewController: UIViewController {
     }
     
     @IBAction func showMessage(sender: UIButton) {
-        let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello World", preferredStyle: UIAlertController.Style.alert)
-        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-        present(alertController, animated: true, completion: nil)
+        // Initialize the dictionary for the emoji icons
+        // If you forgot how to do it, refer to the previous chapter
+        // Fill in the code below
+        var emojiDict = ["👾": "Alien monster", "🤖":"Robot", "🤓":"Nerdy", "👻":"Ghost"]
+        
+        // The sender is the button that is tapped by the user.
+        // Here we store the sender in the selectedButton constant
+        let selectedButton = sender
+        
+        // Get the emoji from the title label of the selected button
+        if let wordToLookup = selectedButton.titleLabel?.text {
+            
+            // Get the meaning of the emoji from the dictionary
+            // Fill in the code below
+            let meaning = emojiDict[wordToLookup]
+        
+            // Change the line below to display the meaning of the emoji instead of Hello World
+            let alertController = UIAlertController(title: "Meaning", message: meaning, preferredStyle: UIAlertController.Style.alert)
+            
+            alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+            present(alertController, animated: true, completion: nil)
+        }
+        
     }
 
 }
